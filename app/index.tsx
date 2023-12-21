19276cd49ac67c2604a550e012e18d5d70d28c51
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-12-21 17:46:35 +0100

    feat: added expo router

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+interface HomePageProps {}
+
+export default function HomePage({}: HomePageProps) {
+  return (
+    <View>
+      <Text>Hello there!</Text>
+    </View>
+  );
+}
