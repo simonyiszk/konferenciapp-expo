5b2a71848b6fc631d0743634389b482b9efe7962
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-01 19:53:54 +0100

    fix: sort presentation by start time

--- services/conference.service.ts
+++ services/conference.service.ts
@@ -1,9 +1,26 @@
+import { isAfter, isBefore } from 'date-fns';
+
 import { axiosInstance } from '../config/axios.config';
 import { FullConferenceDto } from '../types/conference-api.type';
 
 export class ConferenceService {
   static async getConferenceData(): Promise<FullConferenceDto> {
     const response = await axiosInstance.get<FullConferenceDto>('/api/conference/index');
+    response.data.presentations = ConferenceService.sortPresentationsByStartDate(response.data);
     return response.data;
+  }
+
+  private static sortPresentationsByStartDate(conference: FullConferenceDto) {
+    return conference.presentations.sort((a, b) => {
+      const aStartDate = new Date(a.startTime);
+      const bStartDate = new Date(b.startTime);
+      if (isBefore(aStartDate, bStartDate)) {
+        return 1;
+      }
+      if (isAfter(aStartDate, bStartDate)) {
+        return -1;
+      }
+      return 0;
+    });
   }
 }
