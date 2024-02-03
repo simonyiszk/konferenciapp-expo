d7b2f4887ac72f530dbe1be541cf18b0503bf9db
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-03 18:42:16 +0100

    feat: item card base component

---
+++ components/base/item-card.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { GestureResponderEvent, Pressable, PressableProps } from 'react-native';
+
+import { cn } from '../../utils/common.utils';
+
+export function ItemCard({ className, onPressIn, onPressOut, ...props }: PressableProps) {
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePressIn = (event: GestureResponderEvent) => {
+    setIsPressed(true);
+    onPressIn?.(event);
+  };
+
+  const handlePressOut = (event: GestureResponderEvent) => {
+    setIsPressed(false);
+    onPressOut?.(event);
+  };
+  return (
+    <Pressable
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      className={cn(
+        'mb-5 rounded-xl bg-white p-3 shadow-md shadow-slate-500/10',
+        {
+          'bg-slate-50': isPressed,
+        },
+        className
+      )}
+      {...props}
+    />
+  );
+}
--- components/news/elements/news-item.tsx
+++ components/news/elements/news-item.tsx
@@ -1,37 +1,23 @@
 import { useNavigation } from 'expo-router';
-import { useState } from 'react';
-import { Pressable, PressableProps } from 'react-native';
+import { PressableProps } from 'react-native';
 import { NativeStackNavigationProp } from 'react-native-screens/native-stack';
 
 import { NewsItemDto } from '../../../types/news-api.type';
-import { cn } from '../../../utils/common.utils';
 import { formatHu } from '../../../utils/date.utils';
+import { ItemCard } from '../../base/item-card';
 import { StyledText } from '../../base/text';
 
 interface NewsItemProps extends Omit<PressableProps, 'onPress' | 'onPressIn' | 'onPressOut'> {
   newsItem: NewsItemDto;
 }
 
-export function NewsItem({ newsItem, className, ...props }: NewsItemProps) {
+export function NewsItem({ newsItem, ...props }: NewsItemProps) {
   const router = useNavigation<NativeStackNavigationProp<{ 'news-details': { id: string } }>>();
-  const [isPressed, setIsPressed] = useState(false);
   const onPress = () => {
     router.navigate('news-details', { id: newsItem.url });
   };
   return (
-    <Pressable
-      onPress={onPress}
-      onPressIn={() => setIsPressed(true)}
-      onPressOut={() => setIsPressed(false)}
-      className={cn(
-        'mb-5 rounded-xl bg-white space-y-2 p-3 shadow-md shadow-slate-500/10',
-        {
-          'bg-slate-50': isPressed,
-        },
-        className
-      )}
-      {...props}
-    >
+    <ItemCard onPress={onPress} {...props}>
       <StyledText className='text-xl' numberOfLines={2}>
         {newsItem.title}
       </StyledText>
@@ -39,6 +25,6 @@ export function NewsItem({ newsItem, className, ...props }: NewsItemProps) {
       <StyledText className='text-slate-500' numberOfLines={2}>
         {newsItem.briefContent}
       </StyledText>
-    </Pressable>
+    </ItemCard>
   );
 }
