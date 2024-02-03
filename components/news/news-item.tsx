d4e8bb1661a3bbcf95892b23de4d730b8998244c
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-03 17:22:31 +0100

    feat: news list item improvements

--- components/news/news-item.tsx
+++ components/news/news-item.tsx
@@ -1,10 +1,11 @@
 import { useNavigation } from 'expo-router';
 import { useState } from 'react';
-import { Pressable, PressableProps, View } from 'react-native';
+import { Pressable, PressableProps } from 'react-native';
 import { NativeStackNavigationProp } from 'react-native-screens/native-stack';
 
 import { NewsItemDto } from '../../types/news-api.type';
 import { cn } from '../../utils/common.utils';
+import { formatHu } from '../../utils/date.utils';
 import { StyledText } from '../base/text';
 
 interface NewsItemProps extends Omit<PressableProps, 'onPress' | 'onPressIn' | 'onPressOut'> {
@@ -23,7 +24,7 @@ export function NewsItem({ newsItem, className, ...props }: NewsItemProps) {
       onPressIn={() => setIsPressed(true)}
       onPressOut={() => setIsPressed(false)}
       className={cn(
-        'mb-5 rounded-xl bg-white flex-row p-3 items-center shadow-md shadow-slate-500/10',
+        'mb-5 rounded-xl bg-white space-y-2 p-3 shadow-md shadow-slate-500/10',
         {
           'bg-slate-50': isPressed,
         },
@@ -31,11 +32,13 @@ export function NewsItem({ newsItem, className, ...props }: NewsItemProps) {
       )}
       {...props}
     >
-      <View className='flex-col gap-2 flex-1 mx-2'>
-        <StyledText className='text-xl' numberOfLines={1}>
-          {newsItem.title}
-        </StyledText>
-      </View>
+      <StyledText className='text-xl' numberOfLines={2}>
+        {newsItem.title}
+      </StyledText>
+      <StyledText className='text-slate-500'>{formatHu(new Date(newsItem.timestamp))}</StyledText>
+      <StyledText className='text-slate-500' numberOfLines={2}>
+        {newsItem.briefContent}
+      </StyledText>
     </Pressable>
   );
 }
