f5b12c0af7f5da3f16ee3d9dba4656574c15f1e5
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-03 18:29:38 +0100

    feat: skeleton for detail page

--- components/news/layouts/news-details-page.tsx
+++ components/news/layouts/news-details-page.tsx
@@ -4,7 +4,8 @@ import { ScrollContent } from '../../base/scroll-content';
 import { StyledText } from '../../base/text';
 import { ErrorMessage } from '../../common/error-message';
 import { Header } from '../../common/header';
-import { SkeletonRectangle } from '../../common/skeleton';
+import { SkeletonParagraph } from '../../common/skeletons/skeleton-paragraph';
+import { SkeletonTitle } from '../../common/skeletons/skeleton-title';
 import { Title } from '../../common/title';
 
 interface NewsDetailsPageProps {
@@ -17,13 +18,14 @@ export function NewsDetailsPage({ id }: NewsDetailsPageProps) {
   return (
     <Screen>
       <Header>
-        {isLoading && <SkeletonRectangle className='h-10' />}
+        {isLoading && <SkeletonTitle />}
         {data.title && <Title>{data.title}</Title>}
       </Header>
       <ScrollContent>
         {error && (
           <ErrorMessage>Hiba történt a hír betöltése közben. Lehet, hogy ez a hír nem is létezik?</ErrorMessage>
         )}
+        {isLoading && <SkeletonParagraph />}
         <StyledText className='text-xl'>{data?.content}</StyledText>
       </ScrollContent>
     </Screen>
--- components/schedule/layouts/presentation-details-page.tsx
+++ components/schedule/layouts/presentation-details-page.tsx
@@ -5,6 +5,8 @@ import { Screen } from '../../base/screen';
 import { ScrollContent } from '../../base/scroll-content';
 import { StyledText } from '../../base/text';
 import { Header } from '../../common/header';
+import { SkeletonParagraph } from '../../common/skeletons/skeleton-paragraph';
+import { SkeletonTitle } from '../../common/skeletons/skeleton-title';
 import { Subtitle } from '../../common/subtitle';
 import { Title } from '../../common/title';
 import { FavoriteButton } from '../elements/favorite-button';
@@ -14,22 +16,27 @@ interface ScheduleDetailsPageProps {
 }
 
 export function PresentationDetailsPage({ slug }: ScheduleDetailsPageProps) {
-  const { data } = usePresentation(slug);
-  if (!data) return <></>;
-  const startDate = format(new Date(data.startTime), 'HH:mm');
-  const endDate = format(new Date(data.endTime), 'HH:mm');
+  const { data, isLoading } = usePresentation(slug);
+  const startDate = data ? format(new Date(data.startTime), 'HH:mm') : '';
+  const endDate = data ? format(new Date(data.endTime), 'HH:mm') : '';
   return (
     <Screen>
       <Header>
-        <Title>{data?.title}</Title>
-        <Subtitle>
-          {data?.room} • {startDate} - {endDate}
-        </Subtitle>
+        {isLoading && <SkeletonTitle />}
+        {data && (
+          <>
+            <Title>{data?.title}</Title>
+            <Subtitle>
+              {data?.room} • {startDate} - {endDate}
+            </Subtitle>
+          </>
+        )}
       </Header>
       <ScrollContent>
-        <StyledText className='text-xl'>{data?.description}</StyledText>
+        {isLoading && <SkeletonParagraph />}
+        {data && <StyledText className='text-xl'>{data?.description}</StyledText>}
       </ScrollContent>
-      <FavoriteButton presentation={data} />
+      {data && <FavoriteButton presentation={data} />}
     </Screen>
   );
 }
