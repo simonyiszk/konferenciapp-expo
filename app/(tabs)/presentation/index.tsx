6423acc021955279935d1f1d8c52c743332bcc86
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-10 14:12:46 +0100

    feat: added link favorite presentation list screen

--- app/(tabs)/presentation/index.tsx
+++ app/(tabs)/presentation/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'expo-router';
 import React from 'react';
 
 import { Screen } from '../../../components/base/screen';
 import { ErrorMessage } from '../../../components/common/error-message';
 import { Header } from '../../../components/common/header';
+import { StyledButton } from '../../../components/common/styled-button';
 import { Title } from '../../../components/common/title';
 import { PresentationItemSkeletonList } from '../../../components/schedule/layouts/presentation-item-skeleton-list';
 import { PresentationList } from '../../../components/schedule/layouts/presentation-list';
@@ -15,6 +17,11 @@ export default function PresentationListPage() {
       <Header>
         <Title>Programterv</Title>
       </Header>
+      <Link href='/presentation/favorite-presentations' asChild>
+        <StyledButton variant='outline' rightIcon='arrow-right' className='mt-4 mx-5'>
+          Kedvenceim
+        </StyledButton>
+      </Link>
       {isLoading && <PresentationItemSkeletonList />}
       {!isError && !isLoading && <PresentationList presentations={data?.presentations ?? []} />}
       {isError && <ErrorMessage>Nem sikerült betölteni az előadásokat</ErrorMessage>}
