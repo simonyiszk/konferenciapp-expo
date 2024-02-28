49a93be0cb9f1cc77a185158bb42d24d7724df0b
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-02-28 21:03:01 +0100

    chore: current date outsourced

--- utils/presentation.utils.ts
+++ utils/presentation.utils.ts
@@ -2,21 +2,23 @@ import { differenceInMinutes, isAfter, isBefore } from 'date-fns';
 
 import { PresentationDto } from '../types/conference-api.type';
 
+const currentDate = new Date();
+
 export function isPresentationPast(presentation: PresentationDto) {
-  const now = new Date();
+  const now = currentDate;
   const end = new Date(presentation.endTime);
   return isBefore(end, now);
 }
 
 export function isPresentationCurrent(presentation: PresentationDto) {
-  const now = new Date();
+  const now = currentDate;
   const start = new Date(presentation.startTime);
   const end = new Date(presentation.endTime);
   return isBefore(start, now) && isAfter(end, now);
 }
 
 export function isPresentationUpcoming(presentation: PresentationDto) {
-  const now = new Date();
+  const now = currentDate;
   const start = new Date(presentation.startTime);
   return isAfter(start, now) && differenceInMinutes(start, now) < 15;
 }
