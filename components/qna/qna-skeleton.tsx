9b94d06f13f1342dac9673affbae02a29b858b8e
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-15 14:26:29 +0100

    feat: qna skeleton size and radius improved

--- components/qna/qna-skeleton.tsx
+++ components/qna/qna-skeleton.tsx
@@ -1,9 +1,5 @@
 import { SkeletonRectangle } from '../common/skeleton';
 
 export function QnaSkeleton() {
-  return (
-    <>
-      <SkeletonRectangle className='rounded-t-2xl rounded-bl-2xl p-3 mb-2 ml-5' />
-    </>
-  );
+  return <SkeletonRectangle className='rounded-br-none p-3 mb-2 ml-5 h-14' />;
 }
