e65f22dcf7f22ad1a07158bbae58cccc1e6e273a
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-15 18:07:46 +0100

    chore: styled button tests

---
+++ __tests__/components/common/styled-button.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, waitFor } from '@testing-library/react-native';
+
+import { StyledButton } from '../../../components/common/styled-button';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+it('should have children if provided', () => {
+  const { queryByText } = render(<StyledButton>Click me</StyledButton>);
+  expect(queryByText('Click me')).toBeTruthy();
+});
+
+it('should have left icon if provided', async () => {
+  const { queryByTestId } = render(<StyledButton leftIcon='arrow-left' />);
+  await waitFor(() => expect(queryByTestId('left-icon')).toBeTruthy());
+});
+
+it("should not have left icon if it's not provided", () => {
+  const { queryByTestId } = render(<StyledButton />);
+  expect(queryByTestId('left-icon')).toBeFalsy();
+});
+
+it('should have right icon if provided', async () => {
+  const { queryByTestId } = render(<StyledButton rightIcon='arrow-right' />);
+  await waitFor(() => expect(queryByTestId('right-icon')).toBeTruthy());
+});
+
+it("should not have right icon if it's not provided", () => {
+  const { queryByTestId } = render(<StyledButton />);
+  expect(queryByTestId('right-icon')).toBeFalsy();
+});
