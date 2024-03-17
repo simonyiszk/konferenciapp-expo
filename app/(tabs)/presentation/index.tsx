9ae1415a23b8249be954147848853cb5f8fe5ac0
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2024-03-17 13:52:43 +0100

    feat: location filter improvements

--- app/(tabs)/presentation/index.tsx
+++ app/(tabs)/presentation/index.tsx
@@ -1,27 +1,32 @@
 import { Link } from 'expo-router';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { View } from 'react-native'; // Make sure to import View
 
 import { Screen } from '../../../components/base/screen';
 import { ErrorMessage } from '../../../components/common/error-message';
 import { Header } from '../../../components/common/header';
 import { StyledButton } from '../../../components/common/styled-button';
 import { Title } from '../../../components/common/title';
+import { LocationFilter } from '../../../components/schedule/elements/location-filter';
 import { PresentationItemSkeletonList } from '../../../components/schedule/layouts/presentation-item-skeleton-list';
 import { PresentationList } from '../../../components/schedule/layouts/presentation-list';
 import { useConference } from '../../../hooks/use-conference';
 
 export default function PresentationListPage() {
+  const [filter, setFilter] = useState<string>();
   const { data, isError, isLoading } = useConference();
   const { t } = useTranslation();
-  const ButtonNames: string[] = ['Minden előadás', 'IB025', 'IB028'];
-  const [RenderMode, setRenderMode] = useState<number>(0);
-  // Placeholder function for button click
-  const handleButtonClick = (buttonNumber: number) => {
-    //console.log(`Button ${buttonNumber} clicked`);
-    setRenderMode(buttonNumber);
-  };
+  const filterOptions = useMemo(() => {
+    const options = new Set<string>();
+    data?.presentations.forEach((presentation) => options.add(presentation.room));
+    return options;
+  }, [data?.presentations]);
+
+  const filteredPresentations = useMemo(() => {
+    return filter
+      ? data?.presentations?.filter((presentation) => presentation.room === filter) ?? []
+      : data?.presentations ?? [];
+  }, [data?.presentations, filter]);
 
   return (
     <Screen analyticsScreenName='presentation'>
@@ -33,28 +38,9 @@ export default function PresentationListPage() {
           {t('presentations.favorites')}
         </StyledButton>
       </Link>
-      <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', marginTop: 16 }}>
-        {[0, 1, 2].map((buttonNumber) => (
-          <StyledButton
-            key={buttonNumber}
-            variant={buttonNumber === RenderMode ? 'primary' : 'outline'}
-            style={{ margin: 8 }}
-            onPress={() => handleButtonClick(buttonNumber)}
-          >
-            {t(`${ButtonNames[buttonNumber]}`)}
-          </StyledButton>
-        ))}
-      </View>
+      <LocationFilter current={filter} options={[...filterOptions]} onChange={setFilter} className='mt-4' />
       {isLoading && <PresentationItemSkeletonList />}
-      {!isError && !isLoading && (
-        <PresentationList
-          presentations={
-            RenderMode === 0
-              ? data?.presentations ?? []
-              : data?.presentations?.filter((presentation) => presentation.room === ButtonNames[RenderMode]) ?? []
-          }
-        />
-      )}
+      {!isError && !isLoading && <PresentationList presentations={filteredPresentations} />}
       {isError && <ErrorMessage>{t('presentations.error')}</ErrorMessage>}
     </Screen>
   );
