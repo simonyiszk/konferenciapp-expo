8f3966d816aa28a8d844e02b9a90703625dde05a
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2025-05-06 18:34:16 +0200

    feat: posthog env var improvements

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 import * as env from 'env-var';
 config();
 
-const EXPO_PUBLIC_POSTHOG_API_KEY = env.get('EXPO_PUBLIC_POSTHOG_API_KEY').default('').asString();
+const EXPO_PUBLIC_POSTHOG_API_KEY = env.get('EXPO_PUBLIC_POSTHOG_API_KEY').required().asString();
 const EXPO_PUBLIC_EDITION_NAME = env.get('EXPO_PUBLIC_EDITION_NAME').default('').asString();
 
 export default ({ config }: ConfigContext) => {
