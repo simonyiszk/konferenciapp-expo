0a1f1351fd58ba8a6262e4f1877542cb6c8d4dc3
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2026-03-08 17:33:23 +0100

    feat: Add conference date configuration and enhance notification scheduling logic

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,15 @@
 import { ConfigContext } from '@expo/config';
+import { format } from 'date-fns';
 import { config } from 'dotenv';
 import * as env from 'env-var';
 config();
 
 const EXPO_PUBLIC_POSTHOG_API_KEY = env.get('EXPO_PUBLIC_POSTHOG_API_KEY').required().asString();
 const EXPO_PUBLIC_EDITION_NAME = env.get('EXPO_PUBLIC_EDITION_NAME').default('').asString();
+const EXPO_PUBLIC_CONFERENCE_DATE = env
+  .get('EXPO_PUBLIC_CONFERENCE_DATE')
+  .default(format(new Date(), 'yyyy-MM-dd'))
+  .asString();
 
 export default ({ config }: ConfigContext) => {
   return {
@@ -12,6 +17,7 @@ export default ({ config }: ConfigContext) => {
     extra: {
       posthogApiKey: EXPO_PUBLIC_POSTHOG_API_KEY,
       editionName: EXPO_PUBLIC_EDITION_NAME,
+      conferenceDate: EXPO_PUBLIC_CONFERENCE_DATE,
       ...config.extra,
     },
   };
--- utils/date.utils.ts
+++ utils/date.utils.ts
@@ -1,12 +1,14 @@
 import { format, isSameDay, parse } from 'date-fns';
 import { hu } from 'date-fns/locale';
 
+import { CONFERENCE_DATE } from '../config/env.config';
+
 export function formatHu(date: Date): string {
   return format(date, 'MM. dd. - HH:mm', { locale: hu });
 }
 
 export function isConferenceDay(): boolean {
-  const targetDateStr = process.env.EXPO_PUBLIC_CONFERENCE_DATE;
+  const targetDateStr = CONFERENCE_DATE;
   if (!targetDateStr) {
     return true;
   }
@@ -17,4 +19,11 @@ export function isConferenceDay(): boolean {
   }
 
   return isSameDay(new Date(), targetDate);
+}
+
+export function getFullDate(time: string): Date {
+  const [hours, minutes] = time.split(':').map(Number);
+  const date = new Date(CONFERENCE_DATE);
+  date.setHours(hours, minutes, 0, 0);
+  return date;
 }
